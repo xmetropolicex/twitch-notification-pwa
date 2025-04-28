709588f2d01b563d21962cd1d64d589501ba9d2a
xmetropolicex <44262015+xmetropolicex@users.noreply.github.com>
2025-04-29 01:16:53 +0300

    Next.js konfigürasyon dosyası ekleniyor

---
+++ next.config.mjs
@@ -0,0 +1,16 @@
+import withPWA from 'next-pwa';
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['static-cdn.jtvnw.net', 'static.twitchcdn.net'],
+  },
+};
+
+export default withPWA({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development'
+})(nextConfig);
